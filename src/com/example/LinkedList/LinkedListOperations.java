647615cac50731b092921c1d50bdecbcfe3f222f
sanjana <sanjanapareek89@gmail.com>
2017-06-26 15:25:20 +0530

    Added insert and delete functions

--- src/com/example/LinkedList/LinkedListOperations.java
+++ src/com/example/LinkedList/LinkedListOperations.java
@@ -1,7 +1,107 @@
 package com.example.LinkedList;
 
+import sun.awt.image.ImageWatched;
+
 /**
  * Created by sanjana on 26/6/17.
  */
+
 public class LinkedListOperations {
+
+    public class Node{
+        int data;
+        Node next;
+        Node(int value){
+            data = value;
+            next = null;
+        }
+    }
+
+    private int size = 0;
+    private Node head;
+    public LinkedListOperations(){
+        head = null;
+    }
+
+    void insertAtHead(int d){
+        Node newNode = new Node(d);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    void insertAtTail(int d){
+        Node newNode = new Node(d);
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        size++;
+    }
+
+    void insertAtPos(int d, int position){
+        if(position == 1 || head == null){
+            insertAtHead(d);
+            return;
+        }
+        Node newNode = new Node(d);
+        Node temp = head;
+        for(int i = 0; i < position-2; i++){
+           temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        size++;
+    }
+
+    void delete(int position){
+        if(position > size){
+            System.out.println("Invalid value for position");
+            return;
+        }
+        if(head == null){
+            System.out.println("No nodes to delete");
+            return;
+        }
+        if(position == 1 && size == 1){
+            head = null;
+            size--;
+            return;
+        }
+        Node temp = head;
+        for(int i = 0; i < position-2; i++){
+            temp = temp.next;
+        }
+        if(temp.next == null){
+
+        }else{
+
+        }
+        Node del = temp.next;
+        System.out.println("node to delete : " + del.data);
+        temp.next = temp.next.next;
+        del.next = null;
+    }
+
+    void printList(){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+
+    public static void main(String[] args){
+        LinkedListOperations linkedListOperations = new LinkedListOperations();
+        linkedListOperations.insertAtHead(10);
+        linkedListOperations.insertAtHead(20);
+        linkedListOperations.insertAtTail(30);
+        linkedListOperations.insertAtTail(40);
+        linkedListOperations.insertAtPos(50, 3);
+//        linkedListOperations.printList();
+        linkedListOperations.delete(4);
+        linkedListOperations.printList();
+    }
 }
